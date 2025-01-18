31aef0291186cc93b7f003cb28f40019ee8021d7
Cor Hoogendoorn <chiuaua@hotmail.com>
2025-01-18 17:40:28 +0100

    [Modbus Sunspec] Add UInt32 data type
    It appears that for the meter type, acc32 data type is used,
    where in the SunSpec and SolarEdge documentation a UINT32 data
    type is defined. This might lead to values suddenly switching
    from positive to negative.

--- bundles/org.openhab.binding.modbus.sunspec/src/main/java/org/openhab/binding/modbus/sunspec/internal/parser/AbstractBaseParser.java
+++ bundles/org.openhab.binding.modbus.sunspec/src/main/java/org/openhab/binding/modbus/sunspec/internal/parser/AbstractBaseParser.java
@@ -103,6 +103,30 @@ public class AbstractBaseParser {
     }
 
     /**
+     * Extract an optional uint32 value
+     *
+     * @param raw the register array to extract from
+     * @param index the address of the field
+     * @return the parsed value or empty if the field is not implemented
+     */
+    protected Optional<Long> extractOptionalUInt32(ModbusRegisterArray raw, int index) {
+        return ModbusBitUtilities.extractStateFromRegisters(raw, index, ValueType.UINT32).map(DecimalType::longValue)
+                .filter(value -> value != 0);
+    }
+
+    /**
+     * Extract a mandatory uint32 value
+     *
+     * @param raw the register array to extract from
+     * @param index the address of the field
+     * @param def the default value
+     * @return the parsed value or default if the field is not implemented
+     */
+    protected Long extractUInt32(ModbusRegisterArray raw, int index, long def) {
+        return Objects.requireNonNull(extractOptionalUInt32(raw, index).orElse(def));
+    }
+
+    /**
      * Extract an optional scale factor
      *
      * @param raw the register array to extract from
